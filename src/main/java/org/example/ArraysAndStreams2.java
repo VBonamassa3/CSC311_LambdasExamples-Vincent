011a1eba3c6e4efc7f5ae2260bb0100bfab2b270
VBonamassa3 <bonavm@farmingdale.edu>
2025-04-06 23:45:37 -0400

    main commit

--- src/main/java/org/example/ArraysAndStreams2.java
+++ src/main/java/org/example/ArraysAndStreams2.java
@@ -9,22 +9,26 @@ public class ArraysAndStreams2 {
    public static void main(String[] args) {
       String[] strings = 
          {"Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet"};
+      System.out.println();
 
       // display original strings
       System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+      System.out.println();
 
       // strings in uppercase
       System.out.printf("strings in uppercase: %s%n",
          Arrays.stream(strings)             
                .map(String::toUpperCase)   
                .collect(Collectors.toList()));
+      System.out.println();
 
       // strings less than "n" (case insensitive) sorted ascending
       System.out.printf("strings less than n sorted ascending: %s%n",
          Arrays.stream(strings)                            
                .filter(s -> s.compareToIgnoreCase("n") < 0)
                .sorted(String.CASE_INSENSITIVE_ORDER)
-               .collect(Collectors.toList()));             
+               .collect(Collectors.toList()));
+      System.out.println();
 
       // strings less than "n" (case insensitive) sorted descending
       System.out.printf("strings less than n sorted descending: %s%n",
@@ -32,5 +36,27 @@ public class ArraysAndStreams2 {
                .filter(s -> s.compareToIgnoreCase("n") < 0)
                .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
                .collect(Collectors.toList()));
+      System.out.println();
+
+      //stream to filter strings that start with a vowel
+      System.out.printf("strings starting with a vowel descending: %s%n",
+              Arrays.stream(strings)
+                      .filter(s -> s.matches("[aeiou].*"))
+                      .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                      .collect(Collectors.toList()));
+      System.out.println();
+
+      //stream to concatenate (connect) all the strings into a single string
+      System.out.printf("concatenated string: %s%n",
+              Arrays.stream(strings)
+                      .collect(Collectors.joining(", ")));
+      System.out.println();
+
+      //stream to count the number of strings that contain more than 5 characters
+      System.out.printf("number of strings with more than 5 characters: %d%n",
+              Arrays.stream(strings)
+                      .filter(s -> s.length() > 5)
+                      .count());
+      System.out.println();
    }
 }
